5162624ebc5f57e82afd40ef3d64e403cef38234
Kimble <100523850+kimble1337@users.noreply.github.com>
2022-03-14 00:15:01 +0000

    Create banner.h

---
+++ configuration/banner.h
@@ -0,0 +1,12 @@
+using namespace std;
+
+// The banner can be customized to your
+void banner() {
+cout << "███████╗██╗      ██████╗ ██╗    ██╗" << endl;
+cout << "██╔════╝██║     ██╔═══██╗██║    ██║" << endl;
+cout << "█████╗  ██║     ██║   ██║██║ █╗ ██║" << endl;
+cout << "██╔══╝  ██║     ██║   ██║██║███╗██║" << endl;
+cout << "██║     ███████╗╚██████╔╝╚███╔███╔╝" << endl;
+cout << "╚═╝     ╚══════╝ ╚═════╝  ╚══╝╚══╝" << endl; 
+                                                              
+}
